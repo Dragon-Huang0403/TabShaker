14a83d762f3bb2c0f2599369e2ecd6eabfbbfc3f
Dragon <j0918023423@gmail.com>
2022-07-05 20:43:42 +0800

    feat(Weather): basic layout

--- src/Widget/Weather/Weather.tsx
+++ src/Widget/Weather/Weather.tsx
@@ -1,21 +1,112 @@
 import React, { useEffect, useState } from 'react';
+import ReactLoading from 'react-loading';
 import styled from 'styled-components';
-import getWeatherInfoIn36Hours from './weatherApi';
-import getCityName from './openStreetMapApi';
+import getCityData from './openStreetMapApi';
+import {
+  handleWeatherDataByElementType,
+  getWeatherIcon,
+  getWeatherDayByDay,
+  getWeatherDesc,
+  getDayString,
+} from './utils';
+import getWeatherDataByChineseCityName from './weatherApi';
 
 const Wrapper = styled.div`
   background: ${({ theme }) => theme.color.black};
-  color: ${({ theme }) => theme.color.white};
+  color: ${({ theme }) => theme.color.purple};
   width: 100%;
   height: 100%;
+  display: flex;
+`;
+
+const LoadingWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const CurrentWeather = styled.div`
+  height: 100%;
+  width: 140px;
+  display: flex;
+  flex-direction: column;
+  padding-top: 10px;
+  text-align: center;
+`;
+
+const CurrentTemperature = styled.div`
+  margin-top: auto;
+  font-size: 2.5rem;
+`;
+const ApparentTemperature = styled.div`
+  color: ${({ theme }) => theme.color.lavenderBlue};
+  padding: 5px 5px 30px 10px;
+  text-align: center;
+  font-size: 1rem;
+`;
+
+const RightPartWrapper = styled.div`
+  flex-grow: 1;
+  padding: 20px 20px 0 0;
+  text-align: end;
+  display: flex;
+  flex-direction: column;
+`;
+
+const LocationWrapper = styled.div`
+  font-size: 1.125rem;
+`;
+
+const WeatherDescription = styled.div`
+  margin-top: 5px;
+  font-weight: 600;
+  font-size: 1.25rem;
+`;
+
+const WeatherForecastWrapper = styled.div`
+  color: ${({ theme }) => theme.color.lavenderBlue};
+  display: flex;
+  justify-content: flex-end;
+  margin-top: auto;
+  padding-bottom: 20px;
+
+  & > div {
+    width: 100px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  & img {
+    width: 100%;
+  }
 `;
 
 type Location = { lat: number; lon: number };
+type CityData = {
+  chinese: { state: string };
+  english: { state: string; suburb: string };
+};
+
+export type WeatherData = {
+  startTime: Date;
+  endTime: Date;
+  weatherType: {
+    name: string;
+    value: number;
+  };
+  precipitationProbability: number;
+  apparentTemperature: number;
+  temperature: number;
+};
 
 function Weather() {
   const [location, setLocation] = useState<Location | null>(null);
-  const [cityName, setCityName] = useState('');
-  const [weather, setWeather] = useState(null);
+  const [cityData, setCityData] = useState<CityData | null>(null);
+  const [weatherData, setWeatherData] = useState<WeatherData[]>([]);
+
   useEffect(() => {
     navigator.geolocation.getCurrentPosition((pos) => {
       setLocation({
@@ -26,28 +117,71 @@ function Weather() {
   }, []);
   useEffect(() => {
     if (!location) return;
-    const updateCityName = async () => {
-      const osmData = await getCityName(location);
-      if (osmData?.address?.city) {
-        setCityName(osmData.address.city);
-      }
+    const updateCityData = async () => {
+      const newCityData = await getCityData(location);
+      setCityData(newCityData);
     };
-    updateCityName();
-  }, [location]);
+    updateCityData();
+  }, [location?.lat, location?.lon]);
 
   useEffect(() => {
-    if (!cityName) return;
-    const updateWeatherInfo = async () => {
-      const weatherData = await getWeatherInfoIn36Hours(cityName);
-      const newWeather = weatherData?.records?.location?.[0];
-      if (newWeather) {
-        setWeather(newWeather);
+    if (!cityData) return;
+    const updateWeatherData = async () => {
+      const weatherRawData = await getWeatherDataByChineseCityName(
+        cityData.chinese.state,
+      );
+      if (weatherRawData.success === 'true') {
+        const weatherDataByElementType = handleWeatherDataByElementType(
+          weatherRawData?.records?.locations?.[0]?.location?.[0]
+            ?.weatherElement,
+        );
+        const weatherDataByDay = getWeatherDayByDay(weatherDataByElementType);
+        setWeatherData(weatherDataByDay);
       }
     };
-    updateWeatherInfo();
-  }, [cityName]);
-  console.log('weather', weather);
-  return <Wrapper>Weather</Wrapper>;
+    updateWeatherData();
+  }, [cityData]);
+
+  if (!location || !cityData || weatherData.length === 0) {
+    return (
+      <Wrapper>
+        <LoadingWrapper>
+          <ReactLoading type="spin" />
+        </LoadingWrapper>
+      </Wrapper>
+    );
+  }
+
+  return (
+    <Wrapper>
+      <CurrentWeather>
+        <img src={getWeatherIcon(weatherData[0].weatherType.value)} alt="" />
+        <CurrentTemperature>{weatherData[0].temperature}°</CurrentTemperature>
+        <ApparentTemperature>
+          Feel like {weatherData[0].apparentTemperature}°
+        </ApparentTemperature>
+      </CurrentWeather>
+      <RightPartWrapper>
+        <LocationWrapper>{cityData.english.suburb}</LocationWrapper>
+        <WeatherDescription>
+          {getWeatherDesc(weatherData[0].weatherType.value)}
+        </WeatherDescription>
+        <WeatherForecastWrapper>
+          {weatherData.slice(1).map((dayWeather, index) => (
+            // eslint-disable-next-line react/no-array-index-key
+            <div key={index}>
+              <span>{dayWeather.temperature}°</span>
+              <img
+                src={getWeatherIcon(dayWeather.weatherType.value)}
+                alt="weather"
+              />
+              <span>{getDayString(dayWeather.startTime)}</span>
+            </div>
+          ))}
+        </WeatherForecastWrapper>
+      </RightPartWrapper>
+    </Wrapper>
+  );
 }
 
 export default Weather;
--- src/Widget/Weather/openStreetMapApi.ts
+++ src/Widget/Weather/openStreetMapApi.ts
@@ -1,11 +1,30 @@
 const baseUrl = 'https://nominatim.openstreetmap.org/reverse?format=json';
 
-export default async function getCityName(location: {
-  lat: number;
-  lon: number;
-}) {
+type Location = { lat: number; lon: number };
+
+async function getCityDataInChinese(location: Location) {
   const url = `${baseUrl}&lat=${location.lat}&lon=${location.lon}&zoom=18&addressdetails=1&accept-language=TW`;
   const res = await fetch(url);
   const data = await res.json();
   return data;
+}
+async function getCityDataInEnglish(location: Location) {
+  const url = `${baseUrl}&lat=${location.lat}&lon=${location.lon}&zoom=18&addressdetails=1`;
+  const res = await fetch(url);
+  const data = await res.json();
+  return data;
+}
+
+export default async function getCityData(location: Location) {
+  const [cityDataInChinese, cityDatInEnglish] = await Promise.all([
+    getCityDataInChinese(location),
+    getCityDataInEnglish(location),
+  ]);
+  return {
+    chinese: { state: cityDataInChinese.address.state },
+    english: {
+      state: cityDatInEnglish.address.state,
+      suburb: cityDatInEnglish.address.suburb,
+    },
+  };
 }
