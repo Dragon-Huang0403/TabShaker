d63df0efe06d6200095eaebb6fa7c543ee91e5d6
Dragon <j0918023423@gmail.com>
2022-07-17 14:28:25 +0800

    feat: form input transition

--- src/components/FormInput/FormInput.tsx
+++ src/components/FormInput/FormInput.tsx
@@ -14,27 +14,39 @@ const Title = styled.div<{ focused: boolean }>`
     focused ? theme.color.blue : theme.color.lightGrey};
 `;
 
-const Input = styled.input`
+const InputContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
   margin-top: 8px;
-  color: ${({ theme }) => theme.color.white};
-  background: ${({ theme }) => theme.color.littleTransparentBlack};
+  width: 100%;
   border-radius: 4px;
+  overflow: hidden;
+  background: ${({ theme }) => theme.color.littleTransparentBlack};
+`;
+
+const Input = styled.input`
+  color: ${({ theme }) => theme.color.white};
   border: 0;
   outline: none;
   font-size: 0.875rem;
   padding: 6px 8px;
   width: 100%;
+  background: transparent;
   caret-color: ${({ theme }) => theme.color.blue};
 `;
 
 const UnderLine = styled.div<{ focused: boolean }>`
-  width: 100%;
+  width: 1px;
   height: 0;
-  position: absolute;
-  bottom: 0;
+  position: relative;
+  border-bottom: 2px solid transparent;
+  transition: width 0.5s;
   ${({ focused }) =>
     focused &&
     css`
+      width: 100%;
       border-bottom: 2px solid ${({ theme }) => theme.color.blue};
     `}
 `;
@@ -50,19 +62,21 @@ function FormInput({ title, value, onChange }: FormInputProps) {
   return (
     <Wrapper>
       <Title focused={focused}>{title}</Title>
-      <Input
-        onChange={(e) => {
-          onChange(e.target.value);
-        }}
-        value={value}
-        onFocus={() => {
-          setFocus(true);
-        }}
-        onBlur={() => {
-          setFocus(false);
-        }}
-      />
-      <UnderLine focused={focused} />
+      <InputContainer>
+        <Input
+          onChange={(e) => {
+            onChange(e.target.value);
+          }}
+          value={value}
+          onFocus={() => {
+            setFocus(true);
+          }}
+          onBlur={() => {
+            setFocus(false);
+          }}
+        />
+        <UnderLine focused={focused} />
+      </InputContainer>
     </Wrapper>
   );
 }
